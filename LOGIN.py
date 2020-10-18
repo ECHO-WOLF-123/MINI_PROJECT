2cbd76a35ea753d8338e369b880379e25d9caaa6
VIKRAM <vickknguyen123@gmail.com>
2020-10-18 18:12:47 +0530

    Login Box Updated with loginmessage()

--- LOGIN.py
+++ LOGIN.py
@@ -1,28 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
 
-root = Tk()
-root.title("Passport Application")
 
-frame = LabelFrame(root, padx=100, pady=100)
-frame.grid(padx=300, pady=125)
+def loginmessage():
+    messagebox.showinfo("Login", "Login Sucessful")
+    
 
-# User-Name Index 
-UserName_label = Label(frame, text="    Name:*", padx=2, pady=5).grid(row=0,column=0, padx=1, pady=5)
-E = Entry(frame, width=50, borderwidth=3)
-E.grid(row=0, column=1, columnspan=3, padx=10, pady=10)
+def Frame_Login():
+    root = Tk()
+    root.title("Passport Application")
 
-# Password_Index
-Password_label = Label(frame, text="Password:*", padx=2, pady=5).grid(row=1,column=0, padx=1, pady=5)
-E1 = Entry(frame, width=50, borderwidth=3, show="*")
-E1.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
+    frame = LabelFrame(root, padx=100, pady=100)
+    frame.grid(padx=300, pady=125)
 
-# BUTTONS -Submit & -New_User
-Submit_Button = Button(frame, text=" SUBMIT ", padx=10, pady=5, borderwidth=4)
-Submit_Button.grid(row=2, column=2, padx=30, pady=30) 
+    # User-Name Index 
+    UserName_label = Label(frame, text="    Name:*", padx=2, pady=5).grid(row=0,column=0, padx=1, pady=5)
+    E = Entry(frame, width=50, borderwidth=3)
+    E.grid(row=0, column=1, columnspan=3, padx=10, pady=10)
 
+    # Password_Index
+    Password_label = Label(frame, text="Password:*", padx=2, pady=5).grid(row=1,column=0, padx=1, pady=5)
+    E1 = Entry(frame, width=50, borderwidth=3, show="*")
+    E1.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
-New_User_Button = Button(frame, text=" New User ", padx=5, pady=5, borderwidth=4)
-New_User_Button.grid(row=2, column=3, padx=30, pady=30) 
+    # BUTTONS -Submit & -New_User
+    Submit_Button = Button(frame, text=" SUBMIT ", padx=10, pady=5, borderwidth=4, command=loginmessage)
+    Submit_Button.grid(row=2, column=2, padx=30, pady=30) 
 
-root.mainloop()
+    New_User_Button = Button(frame, text=" New User ", padx=5, pady=5, borderwidth=4)
+    New_User_Button.grid(row=2, column=3, padx=30, pady=30) 
+    
+    root.mainloop()
+    
+Frame_Login()   # Calling the Frame_Login function
